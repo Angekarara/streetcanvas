7401330b07e699a84a43ba3a1392a0896e573d09
Angekarara <mbabange2020@gmail.com>
2024-01-14 20:38:17 +0200

    button component

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { ThemeProvider, Button } from "@rneui/themed";
+import theme from "./src/Theme";
+import StandardButton from "./src/components/StandardButton";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <View style={styles.container}>
+        <Text>Open up App.js to p!</Text>
+        <StandardButton color={"primary"} title={"Big"} />
+        <StandardButton color={"secondary"} title={"Small"} />
+        <StandardButton color={"#A7E821"} title={"Small"} size={"lg"} />
+        <View style={styles.buttonContainer}>
+          <StandardButton color={"#A7E821"} title={"My button"} size={"lg"} />
+        </View>
+
+        <StatusBar style="auto" />
+      </View>
+    </ThemeProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonContainer: {
+    width: "80%",
   },
 });
---
+++ src/components/StandardButton/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Button } from "@rneui/themed";
+
+const StandardButton = ({ title, color, size }) => {
+  return <Button title={title} color={color} size={size} />;
+};
+export default StandardButton;
